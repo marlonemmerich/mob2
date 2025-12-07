cc7909fac0ea7557258ba1c1082cc55fa1bbc54a
Marlon Emmerich <marlonemmerich@yahoo.com>
2025-12-07 15:17:55 -0300

    feat: insert dashboard

--- app/src/main/java/com/example/mob2/MainActivity.kt
+++ app/src/main/java/com/example/mob2/MainActivity.kt
@@ -1,20 +1,35 @@
 package com.example.mob2
 
+import android.content.Intent
 import android.os.Bundle
-import androidx.activity.enableEdgeToEdge
+import android.widget.Button
 import androidx.appcompat.app.AppCompatActivity
-import androidx.core.view.ViewCompat
-import androidx.core.view.WindowInsetsCompat
 
 class MainActivity : AppCompatActivity() {
+
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
-        enableEdgeToEdge()
         setContentView(R.layout.activity_main)
-        ViewCompat.setOnApplyWindowInsetsListener(findViewById(R.id.main)) { v, insets ->
-            val systemBars = insets.getInsets(WindowInsetsCompat.Type.systemBars())
-            v.setPadding(systemBars.left, systemBars.top, systemBars.right, systemBars.bottom)
-            insets
+
+        val btnCharacterById = findViewById<Button>(R.id.btnCharacterById)
+        val btnStaff = findViewById<Button>(R.id.btnStaff)
+        val btnHouseStudents = findViewById<Button>(R.id.btnHouseStudents)
+        val btnExitApp = findViewById<Button>(R.id.btnExitApp)
+
+        btnCharacterById.setOnClickListener {
+            // toDo
+        }
+
+        btnStaff.setOnClickListener {
+            // toDo
+        }
+
+        btnHouseStudents.setOnClickListener {
+            // toDo
+        }
+
+        btnExitApp.setOnClickListener {
+            finishAffinity()
         }
     }
 }
